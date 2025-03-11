d9a3edc133a6f1aabb60c97a2bb8a5a3c707657d
vrushalijalgaonkar <jalgaonkar.vrushali16@gmail.com>
2025-03-12 00:04:57 +0900

    add kafka producer configurations

--- src/main/java/com/example/kafka_producer/config/KafkaProducerConfig.java
+++ src/main/java/com/example/kafka_producer/config/KafkaProducerConfig.java
@@ -1,8 +1,18 @@
 package com.example.kafka_producer.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 
 @Configuration
 public class KafkaProducerConfig {
@@ -12,4 +22,28 @@ public class KafkaProducerConfig {
         System.out.println("New topic created!!");
         return new NewTopic("newtopic2", 5, (short) 1);
     }
+
+    @Bean
+    public Map<String, Object> producerConfig(){
+
+        Map<String,Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return props;
+
+    }
+
+    @Bean
+    public ProducerFactory<String,Object> producerFactory(){
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String,Object> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+
 }
